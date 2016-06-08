1b5cfbb340f3f4561fc084aa688a17e0ef31ea46
Mohamed Magdy <magdymohamed1991@gmail.com>
2016-06-08 13:43:36 +0200

    Initial Commit

---
+++ GoEuro/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GoEuro
+//
+//  Created by Mohamed Magdy on 6/8/16.
+//  Copyright © 2016 Magdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
